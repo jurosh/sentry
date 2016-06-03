e6fff74c01cf1e7d8eae9624a99ae45ba105aacf
Jess MacQueen <jess@getsentry.com>
2016-06-03 15:39:00 -0700

    remove unnecessary change

--- src/sentry/web/frontend/auth_login.py
+++ src/sentry/web/frontend/auth_login.py
@@ -10,8 +10,7 @@ from django.views.decorators.cache import never_cache
 
 from sentry import features
 from sentry.constants import WARN_SESSION_EXPIRED
-from sentry.models import (
-    AuthProvider, Organization, OrganizationStatus)
+from sentry.models import AuthProvider, Organization, OrganizationStatus
 from sentry.web.forms.accounts import AuthenticationForm, RegistrationForm
 from sentry.web.frontend.accounts import send_confirm_email
 from sentry.web.frontend.base import BaseView
